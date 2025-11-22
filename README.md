e2be7d34478ebb8342344d0c85aed1cf77b2d776
Yakov <itmeetm@gmail.com>
2025-11-22 16:11:51 +0300

    feat: add full_name to UserCreate

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 source venv/bin/activate
 # habittracker-backend
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+
+
+//ПОДКЛЮЧЕНИЕ К БАЗЕ//
+sudo -u postgres psql -d habittracker
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, EmailStr
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+    full_name: str 
     
 class UserOut(BaseModel):
     id: int
